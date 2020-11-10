0cdd0b4b058c8f91fb3704bafdee63a9b9284c2d
Valentina Robledo <valenrobledo31@gmail.com>
2020-11-10 00:05:38 -0500

    First commit

---
+++ src/components/layout/Nav.vue
@@ -0,0 +1,52 @@
+<template>
+	<div>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+			<router-link class="navbar-item" to="/">
+				<h3 class="title is 3">Talentos</h3>
+			</router-link>
+
+        <a role="button" class="navbar-burger burger" v-bind:class="{'is-active' : isOpen}" @click.prevent="toggle" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu" v-bind:class="{'is-active' : isOpen}">
+        <div class="navbar-start">
+					<router-link class="navbar-item" to="/">Home</router-link>
+        </div>
+
+        <div class="navbar-end">
+        <div class="navbar-item">
+            <div class="buttons">
+            <router-link class="button is-primary" to="/register">
+                <strong>Registrarse</strong>
+            </router-link>
+            <router-link class="button is-light" to="/login">
+                Ingresar
+            </router-link>
+            </div>
+        </div>
+        </div>
+    </div>
+    </nav>
+	</div>
+</template>
+
+<script>
+export default {
+	data (){
+		return{
+			isOpen:false
+		}
+	},
+	methods: {
+		toggle(){
+			const status = !this.isOpen
+			this.isOpen = status
+		}
+	}
+}
+</script>
---
+++ src/views/Auth/Register.vue
@@ -0,0 +1,57 @@
+<template>
+	<div class="container" style="margin-top:50px;">
+			<div class="columns">
+					<div class="columns is-6 is-offset-3">
+						<h3 class="title is-3">Crear una cuenta</h3>
+						<form action="#" @submit.prevent="register">
+							<div class="field">
+								<label class="label">Name</label>
+								<div class="control">
+									<input class="input" type="text" placeholder="e.g Alex Smith" v-model="name">
+								</div>
+							</div>
+
+							<div class="field">
+								<label class="label">Email</label>
+								<div class="control">
+									<input class="input" type="email" placeholder="e.g. alexsmith@gmail.com" v-model="email">
+								</div>
+							</div>
+								<div class="field">
+								<label class="label">Contraseña</label>
+								<div class="control">
+									<input class="input" type="password" v-model="password">
+								</div>
+							</div>
+							<button type="submit" class="button is-primary">Registrar</button>
+						</form>
+						<div class="notification is-danger" v-if="error">
+							{{ error }}
+						</div>
+					</div>
+			</div>
+	</div>
+</template>
+<script>
+export default {
+		data() {
+			return {
+				name:'',
+				email:'',
+				password:'',
+				error:''
+			}
+		},
+		name:'Register',
+		methods: {
+			register(){
+				if(this.name && this.email && this.password){
+					//enviar form
+				}
+				else{
+					this.error='Todos los campos son requeridos'
+				}
+			}
+		}
+}
+</script>
